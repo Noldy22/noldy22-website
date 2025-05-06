7dff2e2ad9aceaae4a11ada0ddca4319748c734c
Noldy22 <minjaanold2222@gmail.com>
2025-05-07 00:09:55 +0300

    6.3

--- user-check.js
+++ user-check.js
@@ -1,5 +1,18 @@
   import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyAWmF_ZmHuxD4beWeJ29rqW-E49BdwQYyE",
+    authDomain: "noldy22-7836c.firebaseapp.com",
+    projectId: "noldy22-7836c",
+    storageBucket: "noldy22-7836c.firebasestorage.app",
+    messagingSenderId: "782608981663",
+    appId: "1:782608981663:web:bfe34cec174ed662060caa",
+    measurementId: "G-HNT78Q9EM6"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   const auth = getAuth();
   onAuthStateChanged(auth, user => {
     if (user) {
