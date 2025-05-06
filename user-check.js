4b377467b1d313d18fe80e36564a1fb1b2c1a071
Noldy22 <minjaanold2222@gmail.com>
2025-05-07 00:12:38 +0300

    6.4

--- user-check.js
+++ user-check.js
@@ -11,8 +11,6 @@
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
   const auth = getAuth();
   onAuthStateChanged(auth, user => {
     if (user) {
